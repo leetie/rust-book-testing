46ce98d2451f055f91f49c8a83b29c3c3da9100b
Jesse Parsons <savvyjesse@aol.com>
2021-01-24 16:18:50 -0500

    fun with tests

--- src/lib.rs
+++ src/lib.rs
@@ -14,12 +14,46 @@ pub fn add_two(a: i32) -> i32 {
     a + 2
 }
 
+// custom messages for failures
+pub fn greeting(name: &str) -> String {
+    // format!("Hello, {}!", name)
+    String::from("Hello")
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            )
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        }
+
+        Guess { value }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn exploration() {
-        assert_eq!(2 + 2, 4);
+    fn it_works() -> Result<(), String> {
+        // writing tests that return Result<T, E> is convenient because it enables the use of the ? operator, letting tests fail if any operation within them returns an Err variant
+        // cant use #[should_panic] with tests that use Result<T, E>. instead, return Err value
+        if 2 + 2 == 5 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal five 🤡"))
+        }
     }
 
     #[test]
@@ -56,6 +90,28 @@ mod tests {
     fn it_adds_two() {
         assert_eq!(4, add_two(2));
         assert_ne!(5, add_two(1));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Jesse");
+        assert!(
+            !result.contains("Jesse"),
+            "Greeting did not contain name, value was `{}`",
+            result
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")] // make sure the panic message matches what is expected, rather than just accepting a panic for any reason. this will pass if the expected message is a substring of the actual message
+    fn greater_than_100() {
+        Guess::new(420);
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be greater than or equal to 1")]
+    fn less_than_one() {
+        Guess::new(0);
     }
     // #[test]
     // fn another() {
